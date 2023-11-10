55a88197ccf286ef740829d5472e376e8ca5cdf9
cleyton21 <cfernando_21@hotmail.com>
2023-11-10 19:02:13 -0300

    passando a logica para os controllers

--- webservice/app/Http/Controllers/UsuarioController.php
+++ webservice/app/Http/Controllers/UsuarioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -32,4 +33,138 @@ class UsuarioController extends Controller
     
         return $user;
     }
+
+    public function cadastro(Request $request)
+    {
+        $data = $request->all();
+
+        $validacao = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validacao->fails()){
+            return $validacao->errors();
+        }
+
+        $imagem = "/perfis/padrao.jpg";
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'imagem' => $imagem
+        ]);
+
+        $user->token = $user->createToken($user->email)->accessToken;
+        $user->imagem = asset($user->imagem);
+
+        return $user;
+    }
+
+    public function usuario(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function perfil(Request $request)
+    {
+        $user = $request->user();
+        $data = $request->all();
+
+        if(isset($data['password'])) {
+            $validacao = Validator::make($data, [
+                'name' => 'required|string|max:255',
+                'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+                'password' => 'required|string|min:6|confirmed',
+            ]);
+            if($validacao->fails()){
+                return $validacao->errors();
+            }
+            $user->password = bcrypt($data['password']);
+
+        }else{
+            $validacao = Validator::make($data, [
+                'name' => 'required|string|max:255',
+                'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            ]);  
+
+            if($validacao->fails()){
+                return $validacao->errors();
+            }
+            $user->name = $data['name'];
+            $user->email = $data['email'];
+        }
+
+        if(isset($data['imagem'])) {
+
+            Validator::extend('base64image', function ($attribute, $value, $parameters, $validator) {
+                $explode = explode(',', $value);
+                $allow = ['png', 'jpg', 'svg','jpeg'];
+                $format = str_replace(
+                    [
+                        'data:image/',
+                        ';',
+                        'base64',
+                    ],
+                    [
+                        '', '', '',
+                    ],
+                    $explode[0]
+                );
+                // check file format
+                if (!in_array($format, $allow)) {
+                    return false;
+                }
+                // check base64 format
+                if (!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $explode[1])) {
+                    return false;
+                }
+                return true;
+            });
+
+            $valiacao = Validator::make($data, [
+                'imagem' => 'base64image',
+
+            ],['base64image'=>'Imagem inválida']);
+
+            if($valiacao->fails()){
+            return $valiacao->errors();
+            }
+
+            $time = time();
+            $diretorioPai = 'perfis';
+            $diretorioImagem = $diretorioPai.DIRECTORY_SEPARATOR.'perfil_id'.$user->id;
+            $ext = substr($data['imagem'], 11, strpos($data['imagem'], ';') - 11);
+            // return $ext;
+            $urlImagem = $diretorioImagem.DIRECTORY_SEPARATOR.$time.'.'.$ext;
+
+            $file = str_replace('data:image/'.$ext.';base64,','',$data['imagem']);
+            // return $file;
+            $file = base64_decode($file);
+
+            if(!file_exists($diretorioPai)){
+                mkdir($diretorioPai,0700);
+            }
+            if($user->imagem) {
+                if(file_exists($user->imagem)){
+                    unlink($user->imagem);
+                }
+            }
+            if(!file_exists($diretorioImagem)){
+                mkdir($diretorioImagem,0700);
+            }
+
+            file_put_contents($urlImagem,$file);
+
+            $user->imagem = $urlImagem;
+        }
+
+        $user->save();
+
+        $user->imagem = asset($user->imagem);
+        $user->token = $user->createToken($user->email)->accessToken;
+        return $user;
+        }
 }
